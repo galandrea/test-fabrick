d4df67888b1c9d672e891e87de728a7b954ca01d
andrea.gala <andrea.gala@deas.it>
2024-04-26 15:52:29 +0200

    initialize project

---
+++ src/main/java/it/gala/test/fabrick/TestFabrickApplication.java
@@ -0,0 +1,13 @@
+package it.gala.test.fabrick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestFabrickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestFabrickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/gala/testfabrick/TestFabrickApplicationTests.java
@@ -0,0 +1,13 @@
+package it.gala.testfabrick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestFabrickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
